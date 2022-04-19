8125aea384a56500b309b077ad4293b8e67fde8a
coIorbIind <labinfpin13@gmail.com>
2022-04-19 23:53:38 +0300

    added profile and recipies

--- first_lab/admin.py
+++ first_lab/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from first_lab.models import Profile, Recipe
 
-# Register your models here.
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "author", "likes_count")
+    search_fields = ("title", )
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    # list_display = ("id", "user__email")
+    pass
--- first_lab/models.py
+++ first_lab/models.py
@@ -1,7 +1,36 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
-# class Recipe(models.Model):
-#     description = models.TextField(verbose_name="Описание рецепта")
-#     likes_count = models.IntegerField(default=0, verbose_name="Количество лайков")
-#     publication_date = models.DateTimeField(auto_now_add=)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="Пользователь")
+    liked_publications = models.ManyToManyField("Recipe", related_name="users_who_liked",
+                                                verbose_name="Понравившиеся рецепты")
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
+class Recipe(models.Model):
+    title = models.CharField(verbose_name="Название", max_length=50)
+    photo = models.ImageField(verbose_name="Фото")
+    ingredients = models.TextField(verbose_name="Ингридиенты")
+    directions = models.TextField(verbose_name="Шаги")
+    publication_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(Profile, verbose_name="Author", on_delete=models.CASCADE)
+
+    def likes_count(self):
+        return self.users_who_liked.count()
